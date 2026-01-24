2bb5c5b452dd2739b549606eca4fe476818540d6
Maxwell Chan <maxmaxchan@gmail.com>
2026-01-24 02:14:42 -0500

    update

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -66,12 +66,11 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "/images/abc-logo.png" },
-      { url: "/images/abc-logo.png", sizes: "32x32", type: "image/png" },
-      { url: "/images/abc-logo.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
     ],
-    apple: "/images/abc-logo.png",
-    shortcut: "/images/abc-logo.png",
+    apple: "/apple-touch-icon.png",
+    shortcut: "/favicon.ico",
   },
   robots: {
     index: true,
