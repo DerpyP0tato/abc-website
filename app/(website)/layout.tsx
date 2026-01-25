0e77574fd0bb41d49f2535f78b33b9cee6b31f6d
Maxwell Chan <maxmaxchan@gmail.com>
2026-01-25 01:28:00 -0500

    update

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -71,14 +71,7 @@ export const metadata: Metadata = {
       "The premier Asian student organization at Binghamton University. Join our Asian club for networking and career development.",
     images: ["/images/abc-logo.png"],
   },
-  icons: {
-    icon: [
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-    ],
-    apple: "/apple-touch-icon.png",
-    shortcut: "/favicon.ico",
-  },
+
   robots: {
     index: true,
     follow: true,
